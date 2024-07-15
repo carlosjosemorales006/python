9828e18648cbd94ffe20c76b960a12dee1f295ca
carlosjosemorales006 <155334948+carlosjosemorales006@users.noreply.github.com>
2024-07-15 15:44:00 -0400

    new

---
+++ operadores/op_logicos.py
@@ -0,0 +1,26 @@
+
+
+# Existen tres operadores logico (basicos)
+# AND , OR y NOT
+
+#AND
+
+resultado1 = True & True # devuelve True
+resultado2 = False & True # devuelve False
+resultado3 = True & False # devuelve false
+resultado4 = False & False # devuelve false
+
+
+#OR
+
+resultado5 = True | True # devuelve True
+resultado6 = False | True # devuelve Treu
+resultado7 = True | False # devuelve True
+resultado8 = False | False # devuelve False
+
+#Not
+
+resultado9 = not False  #devuelve True
+resultado10 = not True # devuelve False
+
+print (resultado10)
