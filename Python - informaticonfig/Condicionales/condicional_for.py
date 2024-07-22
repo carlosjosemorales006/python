7de15222dd3a5d979be6c96430d8119b35a0ac41
carlosjosemorales006 <155334948+carlosjosemorales006@users.noreply.github.com>
2024-07-22 16:48:39 -0400

    continuacion bucke for

--- Python - informaticonfig/Condicionales/condicional_for.py
+++ Python - informaticonfig/Condicionales/condicional_for.py
@@ -15,16 +15,44 @@ while f<10:
     
 for f in range(20,30,2):
     print('Valor de f es: ', f)
-print('fin del ciclo')'''
+print('fin del ciclo')
 
 suma = 0
 
-for f in range(10):
+for f in range(2):
     valor=int(input('Ingrese valor: '))
     suma += valor
 print('La suma es: ',suma)
 
-promedio = suma / 10
+promedio = suma / 2
 
 print('El promedio es: ', promedio)
+
+for i in "Python":
+    print(i, end="")'''
     
+# control del signo @
+    
+'''arroba = "@"
+
+cantidad = 0
+
+mail = input('Ingrese su mail: ')
+
+for f in mail:
+    if f == arroba:
+        cantidad += 1
+
+if cantidad >= 1:
+    print('Mail correcto')
+else:
+    print("Mail incorrecto")'''
+    
+    
+    
+valor = int(input('Elije una table: '))
+print('elijio ver la tabla del: ', valor)
+
+for i in range(1,12):
+    resultado = valor * i
+    print(f'{valor} X {i} = {resultado}') 
