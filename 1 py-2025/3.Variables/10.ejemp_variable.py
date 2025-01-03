35195e5db6d1f686e9567445eebe279996a2a42f
carlosjosemorales006 <155334948+carlosjosemorales006@users.noreply.github.com>
2025-01-03 20:30:09 +0000

    regala y convenciones

--- 1 py-2025/3.Variables/10.ejemp_variable.py
+++ 1 py-2025/3.Variables/10.ejemp_variable.py
@@ -5,7 +5,27 @@ altura = 5.6
 pais = 'Republica Dominicana'
 
 # Aceder a la varibales
-
+print("Valores iniciales: ")
 print(f'mi edad es: {edad} ')
 print ('Mi altura es:', altura, 'pie')
 print('mi pais de nacimineto es:', pais)
+
+# Modificar el valor de una variable
+edad = 33
+altura = 5.7
+
+print("------------------------------------------")
+print("Valores modificados ")
+print(f'mi edad es: {edad} ')
+print ('Mi altura es:', altura, 'pie')
+print('mi pais de nacimineto es:', pais)
+
+# En Pytho el tipo es dinamico 
+
+edad = "Treinta y dos"
+
+print(f'mi edad es: {edad} ')
+
+# No podemos acceder a una variable no declarada, presenta un error en Python
+telefono = '809-550-4510'
+print('Telefono:', telefono)
---
+++ 1 py-2025/3.Variables/14.reglas_variables_conveciones.py
@@ -0,0 +1,38 @@
+# Ejemplos de reglas extrictas
+
+print(f'''
+- como buena practca se usa snake case para la declaracion de las variable (ejemplo_variable)
+      
+- Los nombres de variables pueden tener letras ( Mayuscula o minusculas), digitos, y guiones bajo (_) 
+      
+- El nombre no puede comenzar con digitos 
+      
+- No puede usar palabras reservadas del lenguaje (keyword). ej. for, if, class, try, etc.
+      
+- Python es sensible a mayucula y minuscula (key sentitiive)
+
+
+''')
+
+nombre_usuario = 'Juan Perez'
+#1cccc=0
+
+kclass = "My clase"
+
+nombre = 'Carlos'
+
+Nombre = 'Karla'
+
+
+print(nombre)
+print(Nombre)
+#print(NOMBRE)
+
+# snake case
+
+nombre_completo = 'Carlos Jose Morales De Jesus'
+
+# prefijo y sufijos
+
+es_casado = True
+nombre_txt = "archivo.txt"
